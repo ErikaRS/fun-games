8177038396f4ab3943e344ef8a309ecafeb145ae
Erika Rice Scherpelz <535971+ErikaRS@users.noreply.github.com>
2026-05-03 14:40:30 -0700

    chore: add yarn pull app entrypoint

---
+++ yarn-pull/main.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+import App from "./yarn-pull.jsx";
+
+const container = document.getElementById("root");
+
+if (!container) {
+  throw new Error("Missing #root mount node.");
+}
+
+createRoot(container).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
